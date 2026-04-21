aeec8d2cbde6cb9a483ed3f391dd9ad0c6300cf0
Damien_HAVART <damien.havart@orange.fr>
2026-04-21 16:24:40 +0200

    correction sur les tests jest

--- angular/jest.config.js
+++ angular/jest.config.js
@@ -1,14 +1,60 @@
 // jest.config.ts
+// const { createCjsPreset } = require('jest-preset-angular/presets');
+
+// /** @type {import('jest').Config} */
+// module.exports = {
+//   ...createCjsPreset(),
+//   testEnvironment: 'jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+//   testPathIgnorePatterns: [
+//     '<rootDir>/node_modules/',
+//     '<rootDir>/dist/',
+//     '<rootDir>/src/test.ts', // fichier Karma par défaut
+//   ],
+// };
+
 const { createCjsPreset } = require('jest-preset-angular/presets');
 
-/** @type {import('jest').Config} */
 module.exports = {
   ...createCjsPreset(),
+
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/dist/',
-    '<rootDir>/src/test.ts', // fichier Karma par défaut
+    '<rootDir>/src/test.ts',
   ],
+
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      isolatedModules: true
+    }
+  }
 };
+
+// const { createCjsPreset } = require('jest-preset-angular/presets');
+
+// module.exports = {
+//   ...createCjsPreset(),
+
+//   testEnvironment: 'jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+
+//   testPathIgnorePatterns: [
+//     '<rootDir>/node_modules/',
+//     '<rootDir>/dist/',
+//     '<rootDir>/src/test.ts',
+//   ],
+
+//   transform: {
+//     '^.+\\.(ts|mjs|js|html)$': [
+//       'ts-jest',
+//       {
+//         tsconfig: '<rootDir>/tsconfig.spec.json'
+//       }
+//     ]
+//   }
+// };
